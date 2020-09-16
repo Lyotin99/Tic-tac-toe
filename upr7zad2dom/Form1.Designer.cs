d216e0a3d231f4a3abae168d2b74e37106196368
lyotin44@gmail.com <lyotin44@gmail.com>
2020-09-17 00:46:35 +0300

    comit all

---
+++ upr7zad2dom/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace upr7zad2dom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.b9 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(107, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 33);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "X:";
+            // 
+            // b9
+            // 
+            this.b9.AutoSize = true;
+            this.b9.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.b9.Location = new System.Drawing.Point(275, 351);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(100, 100);
+            this.b9.TabIndex = 5;
+            this.b9.UseVisualStyleBackColor = false;
+            this.b9.Click += new System.EventHandler(this.button11_click);
+            // 
+            // b8
+            // 
+            this.b8.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.b8.Location = new System.Drawing.Point(194, 351);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(100, 100);
+            this.b8.TabIndex = 6;
+            this.b8.UseVisualStyleBackColor = false;
+            this.b8.Click += new System.EventHandler(this.button11_click);
+            // 
+            // b6
+            // 
+            this.b6.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.b6.Location = new System.Drawing.Point(275, 260);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(100, 100);
+            this.b6.TabIndex = 7;
+            this.b6.UseVisualStyleBackColor = false;
+            this.b6.Click += new System.EventHandler(this.button11_click);
+            // 
+            // b5
+            // 
+            this.b5.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.b5.Location = new System.Drawing.Point(194, 260);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(100, 100);
+            this.b5.TabIndex = 8;
+            this.b5.UseVisualStyleBackColor = false;
+            this.b5.Click += new System.EventHandler(this.button11_click);
+            // 
+            // b7
+            // 
+            this.b7.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.b7.Location = new System.Drawing.Point(113, 351);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(100, 100);
+            this.b7.TabIndex = 9;
+            this.b7.UseVisualStyleBackColor = false;
+            this.b7.Click += new System.EventHandler(this.button11_click);
+            // 
+            // b4
+            // 
+            this.b4.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.b4.Location = new System.Drawing.Point(113, 260);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(100, 100);
+            this.b4.TabIndex = 10;
+            this.b4.UseVisualStyleBackColor = false;
+            this.b4.Click += new System.EventHandler(this.button11_click);
+            // 
+            // b3
+            // 
+            this.b3.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.b3.Location = new System.Drawing.Point(275, 169);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(100, 100);
+            this.b3.TabIndex = 11;
+            this.b3.UseVisualStyleBackColor = false;
+            this.b3.Click += new System.EventHandler(this.button11_click);
+            // 
+            // b2
+            // 
+            this.b2.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.b2.Location = new System.Drawing.Point(194, 169);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(100, 100);
+            this.b2.TabIndex = 12;
+            this.b2.UseVisualStyleBackColor = false;
+            this.b2.Click += new System.EventHandler(this.button11_click);
+            // 
+            // b1
+            // 
+            this.b1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.b1.Location = new System.Drawing.Point(113, 169);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(100, 100);
+            this.b1.TabIndex = 13;
+            this.b1.UseVisualStyleBackColor = false;
+            this.b1.Click += new System.EventHandler(this.button11_click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(283, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 33);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "O:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::upr7zad2dom.Properties.Resources.tic_tac;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(521, 513);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ upr7zad2dom/Form1.cs
@@ -0,0 +1,365 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace upr7zad2dom
+{
+    public partial class Form1 : Form
+    {
+        bool istrue = true;
+        int counterX = 0;
+        int counterO=0;
+        int turn = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cleanB()
+        {
+            try
+            {
+
+                b1.Text = "";
+                b2.Text = "";
+                b3.Text = "";
+                b4.Text = "";
+                b5.Text = "";
+                b6.Text = "";
+                b7.Text = "";
+                b8.Text = "";
+                b9.Text = "";
+                b1.Enabled = true;
+                b2.Enabled = true;
+                b3.Enabled = true;
+                b4.Enabled = true;
+                b5.Enabled = true;
+                b6.Enabled = true;
+                b7.Enabled = true;
+                b8.Enabled = true;
+                b9.Enabled = true;
+                turn = 0;
+                istrue = true;
+                
+            }
+            catch { };
+        }//clean button end
+        private void win()
+        {
+            bool winG = false;
+            //horizontalno
+            if (b1.Text == b2.Text && b2.Text == b3.Text && !b1.Enabled)
+            {
+                winG = true;
+            }
+            if (b4.Text == b5.Text && b5.Text == b6.Text && !b4.Enabled)
+            {
+                winG = true;
+            }
+            if (b7.Text == b8.Text && b8.Text == b9.Text && !b7.Enabled)
+            {
+                winG = true;
+            }
+
+            //vertikalno
+            if (b1.Text == b4.Text && b4.Text == b7.Text && !b1.Enabled)
+            {
+                winG = true;
+            }
+            if (b2.Text == b5.Text && b5.Text == b8.Text && !b2.Enabled)
+            {
+                winG = true;
+            }
+            if (b3.Text == b6.Text && b6.Text == b9.Text && !b3.Enabled)
+            {
+                winG = true;
+            }
+            //diagonalno
+            if (b1.Text == b5.Text && b5.Text == b9.Text && !b1.Enabled)
+            {
+                winG = true;
+            }
+            if (b3.Text == b5.Text && b5.Text == b7.Text && !b3.Enabled)
+            {
+                winG = true;
+            }
+          
+
+
+            if (winG)
+            {
+                disableB();//disables buttons
+                String a = " ";
+                if (istrue)
+                {
+                    a = "O";
+                    counterO++;
+                    label2.Text="O:"+ counterO.ToString();
+                
+                }
+                else
+                {
+                    a = "X";
+                    counterX++;
+                    label1.Text= counterX.ToString();
+                    label1.Text = "X:" + counterX.ToString();
+                }
+                MessageBox.Show(a+" Won the game !","Congratulations");
+
+                if (MessageBox.Show("Do you want to play another game?", "Another game?", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+
+                    cleanB();
+                }
+                else
+                {
+                    if (counterX > counterO)
+                    {
+                        MessageBox.Show("Congratulation X you won the series!!!","Winner");
+                    }
+                    else if (counterX < counterO)
+                    {
+                        MessageBox.Show("That's too bad.Try again next time!","You Lost");
+                    }
+                    else MessageBox.Show("No winner in the series","Draw");
+                    Application.Exit();
+                };
+            }
+            else if(turn == 9)
+            {
+               
+                if (MessageBox.Show("The result is draw.Do you want to play another game?", "Another game?", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+
+                    cleanB();
+                }
+                else
+                {
+                    Application.Exit();
+                };
+            }
+            
+
+        }//end of win
+
+        private void disableB()
+        {
+            try
+            {
+                foreach (Control o in Controls)
+                {
+                    Button b = (Button)o;
+                    b.Enabled = false;
+                }
+            }
+            catch { };
+        }//disable button end
+
+        private void button11_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (istrue)
+                b.Text = "X";
+            else b.Text = "O";
+            istrue = !istrue;
+            b.Enabled = false;
+            b.Font = new Font("Microsoft Sans Serif", 30);
+            turn++;
+            win();
+
+            if (!istrue)
+            {
+                CMM();
+            }
+            
+
+
+            
+
+        }
+        private void CMM()
+        {
+            //priority 1:  get tick tac toe
+            //priority 2:  block x tic tac toe
+            //priority 3:  go for mid space
+            //priority 4:  go for corner space
+            //priority 5:  pick open space
+
+            Button move = null;
+
+            //look for tic tac toe opportunities
+            move = look_for_win_or_block("O"); //look for win
+            if (move == null)
+            {
+                move = look_for_win_or_block("X"); //look for block
+                if (move == null)
+                {
+                    move = look_forO();
+                    if (move == null)
+                    {
+                        move = look_for_corner();
+                        if (move == null)
+                        {
+                            move = look_for_open_space();
+                        }//end if
+                    }
+                }//end if
+            }//end if
+
+            move.PerformClick();
+        }//computer move method end
+
+        private Button look_for_open_space()
+        {
+            Console.WriteLine("Looking for open space");
+            Button b = null;
+            foreach (Control c in Controls)
+            {
+                b = c as Button;
+                if (b != null)
+                {
+                    if (b.Text == "")
+                        return b;
+                }//end if
+            }//end if
+
+            return null;
+        }//look for space end
+
+        private Button look_forO()
+        {
+            if(b5.Text=="")
+            {
+                return b5;
+            }
+            return null;
+        }//look for O end
+        private Button look_for_corner()
+        {
+            Console.WriteLine("Looking for corner");
+            if (b1.Text == "O")
+            {
+                if (b3.Text == "")
+                    return b3;
+                if (b9.Text == "")
+                    return b9;
+                if (b7.Text == "")
+                    return b7;
+            }
+
+            if (b3.Text == "O")
+            {
+                if (b1.Text == "")
+                    return b1;
+                if (b9.Text == "")
+                    return b9;
+                if (b7.Text == "")
+                    return b7;
+            }
+
+            if (b9.Text == "O")
+            {
+                if (b1.Text == "")
+                    return b3;
+                if (b3.Text == "")
+                    return b3;
+                if (b7.Text == "")
+                    return b7;
+            }
+
+            if (b7.Text == "O")
+            {
+                if (b1.Text == "")
+                    return b3;
+                if (b3.Text == "")
+                    return b3;
+                if (b9.Text == "")
+                    return b9;
+            }
+
+            if (b1.Text == "")
+                return b1;
+            if (b3.Text == "")
+                return b3;
+            if (b7.Text == "")
+                return b7;
+            if (b9.Text == "")
+                return b9;
+
+            return null;
+        }//look for corner end
+
+        private Button look_for_win_or_block(string mark)
+        {
+            Console.WriteLine("Looking for win or block:  " + mark);
+            //HORIZONTAL TESTS
+            if ((b1.Text == mark) && (b2.Text == mark) && (b3.Text == ""))
+                return b3;
+            if ((b2.Text == mark) && (b3.Text == mark) && (b1.Text == ""))
+                return b1;
+            if ((b1.Text == mark) && (b3.Text == mark) && (b2.Text == ""))
+                return b2;
+
+            if ((b4.Text == mark) && (b5.Text == mark) && (b6.Text == ""))
+                return b6;
+            if ((b5.Text == mark) && (b6.Text == mark) && (b4.Text == ""))
+                return b4;
+            if ((b4.Text == mark) && (b6.Text == mark) && (b5.Text == ""))
+                return b5;
+
+            if ((b7.Text == mark) && (b8.Text == mark) && (b9.Text == ""))
+                return b9;
+            if ((b8.Text == mark) && (b9.Text == mark) && (b7.Text == ""))
+                return b7;
+            if ((b7.Text == mark) && (b9.Text == mark) && (b8.Text == ""))
+                return b8;
+
+            //VERTICAL TESTS
+            if ((b1.Text == mark) && (b4.Text == mark) && (b7.Text == ""))
+                return b7;
+            if ((b4.Text == mark) && (b7.Text == mark) && (b1.Text == ""))
+                return b1;
+            if ((b1.Text == mark) && (b7.Text == mark) && (b4.Text == ""))
+                return b4;
+
+            if ((b2.Text == mark) && (b5.Text == mark) && (b8.Text == ""))
+                return b8;
+            if ((b5.Text == mark) && (b8.Text == mark) && (b2.Text == ""))
+                return b2;
+            if ((b2.Text == mark) && (b8.Text == mark) && (b5.Text == ""))
+                return b5;
+
+            if ((b3.Text == mark) && (b6.Text == mark) && (b9.Text == ""))
+                return b9;
+            if ((b6.Text == mark) && (b9.Text == mark) && (b3.Text == ""))
+                return b3;
+            if ((b3.Text == mark) && (b9.Text == mark) && (b6.Text == ""))
+                return b6;
+
+            //DIAGONAL TESTS
+            if ((b1.Text == mark) && (b5.Text == mark) && (b9.Text == ""))
+                return b9;
+            if ((b5.Text == mark) && (b9.Text == mark) && (b1.Text == ""))
+                return b1;
+            if ((b1.Text == mark) && (b9.Text == mark) && (b5.Text == ""))
+                return b5;
+
+            if ((b3.Text == mark) && (b5.Text == mark) && (b7.Text == ""))
+                return b7;
+            if ((b5.Text == mark) && (b7.Text == mark) && (b3.Text == ""))
+                return b3;
+            if ((b3.Text == mark) && (b7.Text == mark) && (b5.Text == ""))
+                return b5;
+
+            return null;
+        }
+      
+    }
+}
